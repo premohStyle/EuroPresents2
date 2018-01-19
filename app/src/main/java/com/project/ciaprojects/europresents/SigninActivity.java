a13b1798d19589625608890c382e396f0ed18fc2
premohStyle <35307467+premohStyle@users.noreply.github.com>
2018-01-19 19:57:30 +0100

    REGISTRO
    pues eso

--- app/src/main/java/com/project/ciaprojects/europresents/SigninActivity.java
+++ app/src/main/java/com/project/ciaprojects/europresents/SigninActivity.java
@@ -1,14 +1,29 @@
 package com.project.ciaprojects.europresents;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.ViewAnimator;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,18 +31,49 @@ import java.util.Locale;
 
 public class SigninActivity extends AppCompatActivity {
 
+    private static final int SIGNIN = 1;
+    private static final String TAG = "SigninActivity";
     private Button buttonPrev, buttonNext;
     private ViewAnimator viewAnimator;
     private Animation slide_in_left, slide_out_right, slide_in_right, slide_out_left;
+    private short currentPage;
+    private EditText etUser, etPassword, etCPassword, etName, etLastname, etEmail;
+    private LinearLayout contentLayout;
+    private ProgressBar progressBar;
+    FirebaseAuth firebaseAuth;
+    FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        this.currentPage = 1;
+
+        contentLayout = (LinearLayout) findViewById(R.id.content_layout);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
         buttonPrev = (Button) findViewById(R.id.button_back);
         buttonNext = (Button) findViewById(R.id.button_next);
         viewAnimator = (ViewAnimator) findViewById(R.id.view_animator);
+        etEmail = (EditText) findViewById(R.id.et_email);
+        etPassword = (EditText) findViewById(R.id.et_password);
+        etCPassword = (EditText) findViewById(R.id.et_cpassword);
+        etName = (EditText) findViewById(R.id.et_name);
+        etLastname = (EditText) findViewById(R.id.et_lastname);
+        etUser = (EditText) findViewById(R.id.et_user);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    goMainWindow();
+                }
+            }
+        };
+
 
         Locale[] locale = Locale.getAvailableLocales();
         ArrayList<String> countries = new ArrayList<String>();
@@ -49,24 +95,118 @@ public class SigninActivity extends AppCompatActivity {
         slide_in_right = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
         slide_out_left = AnimationUtils.loadAnimation(this, R.anim.slide_out_left);
 
-        buttonPrev.setOnClickListener(new View.OnClickListener(){
+        buttonPrev.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                viewAnimator.setInAnimation(slide_in_left);
-                viewAnimator.setOutAnimation(slide_out_right);
-                viewAnimator.showPrevious();
-            }});
+                if (currentPage != 1) {
+                    viewAnimator.setInAnimation(slide_in_left);
+                    viewAnimator.setOutAnimation(slide_out_right);
+                    viewAnimator.showPrevious();
+                    currentPage--;
+                    if (currentPage == 2) {
+                        buttonNext.setText(R.string.next);
+                    }
+                }
+            }
+        });
 
-        buttonNext.setOnClickListener(new View.OnClickListener(){
+        buttonNext.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                viewAnimator.setInAnimation(slide_in_right);
-                viewAnimator.setOutAnimation(slide_out_left);
-                viewAnimator.showNext();
-            }});
+                if (validateForm() && currentPage != 3) {
+                    viewAnimator.setInAnimation(slide_in_right);
+                    viewAnimator.setOutAnimation(slide_out_left);
+                    viewAnimator.showNext();
+                    currentPage++;
+                    if (currentPage == 3) {
+                        buttonNext.setText(R.string.send);
+                    }
+                } else {
+                    contentLayout.setVisibility(View.INVISIBLE);
+                    progressBar.setVisibility(View.VISIBLE);
+                    registrarUsuario();
+                }
+            }
+        });
 
 
+    }
+
+    private void registrarUsuario() {
+        final String email = etEmail.getText().toString();
+        final String password = etPassword.getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+                                    new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                goMainWindow();
+                                            } else {
+                                                // If sign in fails, display a message to the user.
+                                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                                Toast.makeText(SigninActivity.this, "Authentication failed.",
+                                                        Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SigninActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        ;
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        if (this.currentPage == 1) {
+            if (etEmail.getText().toString().isEmpty()) {
+                Toast.makeText(this, R.string.validation_empty_fields, Toast.LENGTH_SHORT).show();
+                valid = false;
+            } else if (etPassword.getText().toString().isEmpty()) {
+                Toast.makeText(this, R.string.validation_empty_fields, Toast.LENGTH_SHORT).show();
+                valid = false;
+            } else if (etCPassword.getText().toString().isEmpty()) {
+                Toast.makeText(this, R.string.validation_empty_fields, Toast.LENGTH_SHORT).show();
+                valid = false;
+            } else if (!etPassword.getText().toString().equals(etCPassword.getText().toString())) {
+                Toast.makeText(this, R.string.validation_password_not_match, Toast.LENGTH_SHORT).show();
+                valid = false;
+            }
+        } else if (this.currentPage == 2) {
+            if (etUser.getText().toString().isEmpty()) {
+                Toast.makeText(this, R.string.validation_empty_fields, Toast.LENGTH_SHORT).show();
+                valid = false;
+            } else if (etName.getText().toString().isEmpty()) {
+                Toast.makeText(this, R.string.validation_empty_fields, Toast.LENGTH_SHORT).show();
+                valid = false;
+            } else if (etLastname.getText().toString().isEmpty()) {
+                Toast.makeText(this, R.string.validation_empty_fields, Toast.LENGTH_SHORT).show();
+                valid = false;
+            }
+        } else if (this.currentPage == 3) {
+            valid = false;
+        } else {
+            valid = false;
+        }
+        return valid;
+    }
+
+    private void goMainWindow() {
+        Intent intent = new Intent(this, MainWindowActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
